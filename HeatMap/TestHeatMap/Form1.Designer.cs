b6d202d65340e700dc8fd5dab5c52ae2d6673496
hmfly <hmflying@gmail.com>
2012-09-11 17:25:22 +0800

    it's ok

---
+++ HeatMap/TestHeatMap/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace TestHeatMap
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblOpacity = new System.Windows.Forms.Label();
+            this.lblRadius = new System.Windows.Forms.Label();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.tbOpacity = new System.Windows.Forms.TrackBar();
+            this.tbHPRadius = new System.Windows.Forms.TrackBar();
+            this.tbHPCount = new System.Windows.Forms.TrackBar();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.cmbColorRamp = new System.Windows.Forms.ComboBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tbOpacity)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbHPRadius)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbHPCount)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSize = true;
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.Controls.Add(this.pictureBox3);
+            this.panel1.Controls.Add(this.linkLabel1);
+            this.panel1.Controls.Add(this.cmbColorRamp);
+            this.panel1.Controls.Add(this.lblOpacity);
+            this.panel1.Controls.Add(this.lblRadius);
+            this.panel1.Controls.Add(this.lblCount);
+            this.panel1.Controls.Add(this.tbOpacity);
+            this.panel1.Controls.Add(this.tbHPRadius);
+            this.panel1.Controls.Add(this.tbHPCount);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 662);
+            this.panel1.MaximumSize = new System.Drawing.Size(1200, 80);
+            this.panel1.MinimumSize = new System.Drawing.Size(1200, 80);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1200, 80);
+            this.panel1.TabIndex = 0;
+            // 
+            // lblOpacity
+            // 
+            this.lblOpacity.AutoSize = true;
+            this.lblOpacity.Location = new System.Drawing.Point(887, 56);
+            this.lblOpacity.Name = "lblOpacity";
+            this.lblOpacity.Size = new System.Drawing.Size(41, 12);
+            this.lblOpacity.TabIndex = 6;
+            this.lblOpacity.Text = "label3";
+            // 
+            // lblRadius
+            // 
+            this.lblRadius.AutoSize = true;
+            this.lblRadius.Location = new System.Drawing.Point(781, 56);
+            this.lblRadius.Name = "lblRadius";
+            this.lblRadius.Size = new System.Drawing.Size(41, 12);
+            this.lblRadius.TabIndex = 5;
+            this.lblRadius.Text = "label2";
+            // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.Location = new System.Drawing.Point(375, 59);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(41, 12);
+            this.lblCount.TabIndex = 4;
+            this.lblCount.Text = "label1";
+            // 
+            // tbOpacity
+            // 
+            this.tbOpacity.LargeChange = 1;
+            this.tbOpacity.Location = new System.Drawing.Point(878, 8);
+            this.tbOpacity.Name = "tbOpacity";
+            this.tbOpacity.Size = new System.Drawing.Size(100, 45);
+            this.tbOpacity.TabIndex = 2;
+            this.tbOpacity.Value = 10;
+            this.tbOpacity.Scroll += new System.EventHandler(this.tbOpacity_Scroll);
+            // 
+            // tbHPRadius
+            // 
+            this.tbHPRadius.Location = new System.Drawing.Point(772, 8);
+            this.tbHPRadius.Maximum = 50;
+            this.tbHPRadius.Minimum = 5;
+            this.tbHPRadius.Name = "tbHPRadius";
+            this.tbHPRadius.Size = new System.Drawing.Size(100, 45);
+            this.tbHPRadius.TabIndex = 1;
+            this.tbHPRadius.TickFrequency = 5;
+            this.tbHPRadius.Value = 25;
+            this.tbHPRadius.Scroll += new System.EventHandler(this.tbHPRadius_Scroll);
+            // 
+            // tbHPCount
+            // 
+            this.tbHPCount.LargeChange = 100;
+            this.tbHPCount.Location = new System.Drawing.Point(12, 8);
+            this.tbHPCount.Maximum = 2000;
+            this.tbHPCount.Minimum = 1;
+            this.tbHPCount.Name = "tbHPCount";
+            this.tbHPCount.Size = new System.Drawing.Size(754, 45);
+            this.tbHPCount.SmallChange = 10;
+            this.tbHPCount.TabIndex = 0;
+            this.tbHPCount.TickFrequency = 100;
+            this.tbHPCount.Value = 111;
+            this.tbHPCount.Scroll += new System.EventHandler(this.tbHPCount_Scroll);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox2, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1184, 662);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(586, 656);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox2.Location = new System.Drawing.Point(595, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(586, 656);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // cmbColorRamp
+            // 
+            this.cmbColorRamp.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbColorRamp.FormattingEnabled = true;
+            this.cmbColorRamp.Items.AddRange(new object[] {
+            "THERMAL",
+            "RAINBOW",
+            "RED_WHITE_BLUE"});
+            this.cmbColorRamp.Location = new System.Drawing.Point(1042, 22);
+            this.cmbColorRamp.Name = "cmbColorRamp";
+            this.cmbColorRamp.Size = new System.Drawing.Size(121, 20);
+            this.cmbColorRamp.TabIndex = 7;
+            this.cmbColorRamp.SelectedIndexChanged += new System.EventHandler(this.cmbColorRamp_SelectedIndexChanged);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(1050, 45);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(107, 12);
+            this.linkLabel1.TabIndex = 2;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "http://hmfly.info";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(994, 22);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(40, 40);
+            this.pictureBox3.TabIndex = 8;
+            this.pictureBox3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1184, 742);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.panel1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1200, 781);
+            this.MinimumSize = new System.Drawing.Size(1200, 781);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "HeatMap";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tbOpacity)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbHPRadius)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbHPCount)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TrackBar tbOpacity;
+        private System.Windows.Forms.TrackBar tbHPRadius;
+        private System.Windows.Forms.TrackBar tbHPCount;
+        private System.Windows.Forms.Label lblOpacity;
+        private System.Windows.Forms.Label lblRadius;
+        private System.Windows.Forms.Label lblCount;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ComboBox cmbColorRamp;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+
+
+    }
+}
+
---
+++ HeatMap/TestHeatMap/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using HeatMap;
+using TestHeatMap.Properties;
+
+namespace TestHeatMap
+{
+    public partial class Form1 : Form
+    {
+        int _width;
+        int _height;
+        int _count;
+        float _opacity;
+        int _radius;
+        ColorRamp _colorRamp;
+        List<HeatPoint> _points;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<HeatPoint> randomPoints(int width, int height, int count)
+        {
+            var result = new List<HeatPoint>();
+
+            var r = new Random();
+            for (int i = 0; i < count; i++)
+            {
+                var x = r.Next(width);
+                var y = r.Next(height);
+                //var w = (float)r.NextDouble()/2;
+
+                var point = new HeatPoint
+                {
+                    X = x,
+                    Y = y,
+                    W = 1
+                };
+
+                result.Add(point);
+            }
+
+            return result;
+        }
+
+        private async void make4Maps(int width, int height, int radius, List<HeatPoint> points, float opacity, ColorRamp cr)
+        {
+            var hmMaker = new HeatMapMaker
+            {
+                Width = width,
+                Height = height,
+                Radius = radius,
+                ColorRamp = cr,
+                HeatPoints = points,
+                Opacity = opacity
+            };
+            this.pictureBox2.BackgroundImage = await hmMaker.MakeHeatMap();
+
+            //
+            this.pictureBox1.BackgroundImage = hmMaker.GrayMap;
+
+            //
+            displayInfo();
+        }
+
+        private void displayInfo()
+        {
+            this.lblCount.Text = String.Format("{0}:{1}", "Point Count", _count);
+            this.lblRadius.Text = String.Format("{0}:{1}", "Point Radius", _radius);
+            this.lblOpacity.Text = String.Format("{0}:{1}", "Map Opacity", _opacity);
+        }
+
+        private void tbHPCount_Scroll(object sender, EventArgs e)
+        {
+            _count = this.tbHPCount.Value;
+            _points = randomPoints(_width, _height, _count);
+            make4Maps(_width, _height, _radius, _points, _opacity, _colorRamp);
+        }
+
+        private void tbHPRadius_Scroll(object sender, EventArgs e)
+        {
+            _radius = this.tbHPRadius.Value;
+            make4Maps(_width, _height, _radius, _points, _opacity, _colorRamp);
+        }
+
+        private void tbOpacity_Scroll(object sender, EventArgs e)
+        {
+            _opacity = this.tbOpacity.Value/10f;
+            make4Maps(_width, _height, _radius, _points, _opacity, _colorRamp);
+        }
+
+        private void cmbColorRamp_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (this.cmbColorRamp.SelectedIndex)
+            {
+                case 0:
+                    this.pictureBox3.BackgroundImage = Resources.i_thermal;
+                    break;
+                case 1:
+                    this.pictureBox3.BackgroundImage = Resources.i_rainbow;
+                    break;
+                case 2:
+                    this.pictureBox3.BackgroundImage = Resources.i_redwhiteblue;
+                    break;
+            }
+
+            _colorRamp = (ColorRamp)Enum.Parse(typeof(ColorRamp), this.cmbColorRamp.SelectedItem.ToString());
+
+            make4Maps(_width, _height, _radius, _points, _opacity, _colorRamp);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _width = this.pictureBox1.Width;
+            _height = this.pictureBox1.Height;
+            _opacity = this.tbOpacity.Value / 10f;
+            _count = this.tbHPCount.Value;
+            _radius = this.tbHPRadius.Value;
+            _points = randomPoints(_width, _height, _count);
+            this.cmbColorRamp.SelectedIndex = 1;
+            _colorRamp = (ColorRamp)Enum.Parse(typeof(ColorRamp), this.cmbColorRamp.SelectedItem.ToString());
+            make4Maps(_width, _height, _radius, _points, _opacity, _colorRamp);
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://goo.gl/fwM4Y");
+        }
+    }
+}
